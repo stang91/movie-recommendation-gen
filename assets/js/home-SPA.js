6974662da2b8bfde5a42ac2d2ba041c9c24b922f
Zh3RM <shuzhufilms@gmail.com>
2022-08-18 03:31:35 -0700

    improve formatting. minor debug

--- assets/js/home-SPA.js
+++ assets/js/home-SPA.js
@@ -1,3 +1,6 @@
+var movieID;
+var randomBank = [];
+
 function getHome(event) {
     $(document).prop("title", "Home | Flick Genie");
     $("#content").empty();
@@ -32,6 +35,7 @@ function getHome(event) {
             "data-id": "generate-btn-container",
             "onclick": "getRec(movieID, event)"
         }).append($("<a>generate</a>").attr("class", "btn waves-effect waves-teal generate-btn")),
+
         //dropdown
         $("<ul></ul>").attr({
             "id": "search-dropdown",
@@ -54,9 +58,9 @@ function getHome(event) {
             )));
 
     var searchBarEl = document.querySelector(".search-bar-input");
-    searchBarEl.addEventListener("keydown", generateDropdown);
-
     var searchDropdownEl = document.getElementById("search-dropdown");
+
+    searchBarEl.addEventListener("keydown", generateDropdown);
 
     function generateDropdown() {
         var movieTitle = searchBarEl.value;
@@ -69,11 +73,11 @@ function getHome(event) {
                 return response.json();
             })
             .then(function (data) {
-
                 // console.log(data);
                 searchDropdownEl.innerHTML = '';
                 searchDropdownEl.classList.remove("hide");
 
+                // for the autocomplete parameters of this materialize element, for the data attribute, it has to be a JSON not string. We also don't need this because it only contains two attributes, the title and the poster link, formatted in JSON. Can't use this to pass movie_ID
                 // if (data.results.length){
                 // for (i = 0; i < data.results.length; i++) {
                 //     $('input.autocomplete').autocomplete({
@@ -88,18 +92,19 @@ function getHome(event) {
                     for (i = 0; i < 5; i++) {
                         var dropDownItem = document.createElement("li");
                         dropDownItem.classList.add("drop-down-item");
-                        dropDownItem.setAttribute("data-ID", data.results[i].id)
+                        dropDownItem.setAttribute("data-ID", data.results[i].id);
                         dropDownItem.setAttribute("data-title", data.results[i].original_title);
+
                         var title = document.createElement("p");
-                        var poster = document.createElement("img");
-                        var releaseDate = document.createElement("p");
                         title.textContent = data.results[i].original_title;
 
+                        var releaseDate = document.createElement("p");
+                        releaseDate.textContent = data.results[i].release_date.substring(0, 4);
+
+                        var poster = document.createElement("img");
                         if (data.results[i].poster_path) {
                             poster.setAttribute("src", 'https://image.tmdb.org/t/p/w500' + data.results[i].poster_path);
                         }
-
-                        releaseDate.textContent = data.results[i].release_date.substring(0, 4);
 
                         dropDownItem.append(title, releaseDate, poster);
                         searchDropdownEl.append(dropDownItem);
@@ -107,14 +112,10 @@ function getHome(event) {
                         title.addEventListener("click", getMovieID)
                         releaseDate.addEventListener("click", getMovieID)
                         poster.addEventListener("click", getMovieID)
-
-
                     }
                 }
             })
     }
-
-
 
     function getMovieID(event) {
         movieID = event.target.parentElement.getAttribute("data-ID");
@@ -125,11 +126,12 @@ function getHome(event) {
         return movieID, event, searchBarEl.value;
     }
 
-    getRec(movieID, event)
+
+    //basically load the poster cards using stored movie_ID and randomIndex when home button is clicked.
+    getRec(movieID, event);
 
 }
-var movieID
-var randomBank = [];
+
 
 
 function getRec(movieID, event) {
@@ -139,6 +141,7 @@ function getRec(movieID, event) {
     cardDeckEl.innerHTML = '';
 
     var movieDBRecURL = "https://api.themoviedb.org/3/movie/" + movieID + "/recommendations?api_key=" + themoviedbAPIKey
+
     fetch(movieDBRecURL, {
         method: 'GET',
     })
@@ -147,27 +150,30 @@ function getRec(movieID, event) {
         })
         .then(function (data) {
 
-            if (event.target.classList.contains("home-btn")) { generateCards(data) } else {
+            //when generating recommendation, if home button was clicked, we keep the same random Index effectively loading the same posters generated before
+            if (event.target.classList.contains("home-btn")) { generateCards(data) }
+            else {
                 getRandomIndex(data)
                 generateCards(data)
             };
 
-            function generateCards(data) { }
-            for (i = 0; i < 4; i++) {
-                randomBank.push(randomBank[i]);
-                var posterDisplay = document.createElement("div");
-                posterDisplay.classList.add("card-container");
-                posterDisplay.innerHTML = "<img class='card-image' data-title='" + data.results[randomBank[i]].title + "' src=https://image.tmdb.org/t/p/w500" + data.results[randomBank[i]].poster_path + "> <p class='card-text'>" + data.results[randomBank[i]].title + "<p>"
-                posterDisplay.setAttribute("data-ID", data.results[randomBank[i]].id)
-                posterDisplay.setAttribute("data-title", data.results[randomBank[i]].title)
-                cardDeckEl.append(posterDisplay);
+            function generateCards(data) {
+                for (i = 0; i < 4; i++) {
+                    randomBank.push(randomBank[i]);
+                    var posterDisplay = document.createElement("div");
+                    posterDisplay.classList.add("card-container");
+                    posterDisplay.innerHTML = "<img class='card-image' data-title='" + data.results[randomBank[i]].title + "' src=https://image.tmdb.org/t/p/w500" + data.results[randomBank[i]].poster_path + "> <p class='card-text'>" + data.results[randomBank[i]].title + "<p>"
+                    posterDisplay.setAttribute("data-ID", data.results[randomBank[i]].id)
+                    posterDisplay.setAttribute("data-title", data.results[randomBank[i]].title)
+                    cardDeckEl.append(posterDisplay);
 
-                posterDisplay.addEventListener("click", generateMovie);
+                    posterDisplay.addEventListener("click", generateMovie);
 
+                }
             }
 
 
-            return randomBank
+            return randomBank;
         })
 
 
