5e23d14272aea31ec958a4bc2579c801c303b9b0
Jeanette Conklin <conklinj09@gmail.com>
2022-08-18 20:10:41 -0700

    keep files

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # movie-recommendation-gen
+
+We have created an application collaboratively called:
+Flick Genie
+
+Purpose:
+
+This application was made to be a Movie Suggestion Generator.
+It helps the user find movies that they search for through the navigation application. 
+Or program uses two free API interfaces to generate the search results.
+It also contains a short About me and a Carousel at the bottom which shows the user's search history/recent findings. 
+
+
+Here are the websites we used to reference our code:
+we3schools.com
+geeks4geeks.org
+
+
+
+used to help create the Carousel for LocalItemStorage
+https://blog.logrocket.com/build-image-carousel-from-scratch-vanilla-javascript/
--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
     swipeable: true,
     responsiveThreshold: Infinity 
   });
+  // $('.carousel').carousel();
 });
 $(".dropdown-trigger").dropdown();
 //materialize suggested this code for dropdown//
@@ -24,13 +25,83 @@ function resizeTab(){
   $(".tabs-content").css('height',maxHeight+'px');
 }
 
-//materialize template for carousel -- recently viewed
-$(document).ready(function(){
-  $('.carousel').carousel();
-});
-
 // nav slidebar
 $(document).ready(function(){
   $('.sidenav').sidenav();
 });
+
+//Local Item Storage 
+if (sessionStorage.clickcount) {
+  sessionStorage.clickcount = Number(sessionStorage.clickcount) + 1;
+} else {
+  sessionStorage.clickcount = 1;
+}
+document.getElementById("result").innerHTML = "You have clicked the button " +
+sessionStorage.clickcount + " time(s) in this session.";
+
+
+
+//Carousel 
+
+// document.addEventListener('DOMContentLoaded', function() {
+//   var elems = document.querySelectorAll('.carousel');
+//   var instances = M.Carousel.init(elems, options);
+// });
+
+  
+//  let instance.next(3); // Move next n times.
+
+//  else instance.prev(3); // Move previous n times.
+
+
+
+// // Select all slides
+// const slides = document.querySelectorAll(".slide");
+
+// // loop through slides and set each slides translateX property to index * 100% 
+// slides.forEach((slide, indx) => {
+//   slide.style.transform = `translateX(${indx * 100}%)`;
+// });
+
+
+// // select next slide button
+// const nextSlide = document.querySelector(".btn-next");
+
+// // current slide counter
+// let curSlide = 0;
+// // maximum number of slides
+// let maxSlide = slides.length - 1;
+
+// // add event listener and navigation functionality
+// nextSlide.addEventListener("click", function () {
+//   // check if current slide is the last and reset current slide
+//   if (curSlide === maxSlide) {
+//     curSlide = 0;
+//   } else {
+//     curSlide++;
+//   }
+
+// //   move slide by -100%
+//   slides.forEach((slide, indx) => {
+//     slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+//   });
+// });
+
+// // select prev slide button
+// const prevSlide = document.querySelector(".btn-prev");
+
+// // add event listener and navigation functionality
+// prevSlide.addEventListener("click", function () {
+//   // check if current slide is the first and reset current slide to last
+//   if (curSlide === 0) {
+//     curSlide = maxSlide;
+//   } else {
+//     curSlide--
+//   }
+
+//   //   move slide by 100%
+//   slides.forEach((slide, indx) => {
+//     slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+//   });
+// });
 
