82fc65b211df346f6bce5fc633fe2afcfd941eb6
Jeanette Conklin <conklinj09@gmail.com>
2022-08-18 21:17:55 -0700

    feature/carousel

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,58 @@
-$(document).ready(function(){
+$(document).ready(function () {
   $('.tabs').tabs({
     swipeable: true,
-    responsiveThreshold: Infinity 
+    responsiveThreshold: Infinity
+  });
+
+  // Select all slides
+  const slides = document.querySelectorAll(".slide");
+
+  // loop through slides and set each slides translateX property to index * 100% 
+  slides.forEach((slide, indx) => {
+    slide.style.transform = `translateX(${indx * 100}%)`;
+  });
+
+
+
+  // select next slide button
+  const nextSlide = document.querySelector(".btn-next");
+
+  // current slide counter
+  let curSlide = 0;
+  // maximum number of slides
+  let maxSlide = slides.length - 1;
+
+  // add event listener and navigation functionality
+  nextSlide.addEventListener("click", function () {
+    // check if current slide is the last and reset current slide
+    if (curSlide === maxSlide) {
+      curSlide = 0;
+    } else {
+      curSlide++;
+    }
+
+    //   move slide by -100%
+    slides.forEach((slide, indx) => {
+      slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+    });
+  });
+
+  // select prev slide button
+  const prevSlide = document.querySelector(".btn-prev");
+
+  // add event listener and navigation functionality
+  prevSlide.addEventListener("click", function () {
+    // check if current slide is the first and reset current slide to last
+    if (curSlide === 0) {
+      curSlide = maxSlide;
+    } else {
+      curSlide--;
+    }
+
+    //   move slide by 100%
+    slides.forEach((slide, indx) => {
+      slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+    });
   });
 });
 $(".dropdown-trigger").dropdown();
@@ -12,25 +63,25 @@ $(".dropdown-trigger").dropdown();
 
 //https://github.com/Dogfalo/materialize/issues/4148
 //from CiccaGuru
-$(function(){
+$(function () {
   resizeTab();
-  $( window ).resize(function() { resizeTab(); });
+  $(window).resize(function () { resizeTab(); });
 });
-function resizeTab(){
+function resizeTab() {
   var maxHeight = 0;
-  $('.carousel-item').each(function(){ 
-       if($(this).height() > maxHeight) maxHeight = $(this).height(); 
+  $('.carousel-item').each(function () {
+    if ($(this).height() > maxHeight) maxHeight = $(this).height();
   });
-  $(".tabs-content").css('height',maxHeight+'px');
+  $(".tabs-content").css('height', maxHeight + 'px');
 }
 
 //materialize template for carousel -- recently viewed
-$(document).ready(function(){
+$(document).ready(function () {
   $('.carousel').carousel();
 });
 
 // nav slidebar
-$(document).ready(function(){
+$(document).ready(function () {
   $('.sidenav').sidenav();
 });
 
