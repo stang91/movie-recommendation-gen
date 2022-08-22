d465544437ecf7d6d78598a4fa69ed451ca679dd
Zh3RM <shuzhufilms@gmail.com>
2022-08-22 11:16:46 -0700

    create dynamic URL. Improve recently viewed item display. Improve about page css layout. Debug Substring error

--- assets/js/home-SPA.js
+++ assets/js/home-SPA.js
@@ -14,7 +14,7 @@ function getHome(event) {
             })
         ),
 
-     //search bar
+        //search bar
         $("<div></div>").attr({
             "class": "homePage col s12",
             "id": "home"
@@ -70,23 +70,29 @@ function getHome(event) {
             $("<h3>Recently Viewed</h3>"),
             $("<div></div>").attr("class", "rv-item-container")
         )
-   );
-   //creating recent views
-   for(var i=0;i<localStorage.length;i++){
-       var recentView =JSON.parse(localStorage.getItem(localStorage.key(i)));
-       $(".rv-item-container").append(
-           $("<img>").attr({
+    );
+    //creating recent views
+    for (var i = 0; i < localStorage.length; i++) {
+        var recentView = JSON.parse(localStorage.getItem(localStorage.key(i)));
+        if (recentView.poster_path !== null) {
+        $(".rv-item-container").append(
+            $("<a></a>").attr({
+                "href": "#"+ recentView.title.replaceAll(" ", "-"),
                 "class": "rv-item",
-                "id": recentView.id,
                 "data-ID": recentView.id,
-                "data-title": recentView.title,
-                "src":"https://image.tmdb.org/t/p/w500/" + recentView.poster_path,
-            })
-       );
-    };
+                "data-title": recentView.title
+            }).append(
+                $("<img>").attr({
+                    "class": "rv-item",
+                    "id": recentView.id,
+                    "data-ID": recentView.id,
+                    "data-title": recentView.title,
+                    "src": "https://image.tmdb.org/t/p/w500/" + recentView.poster_path,
+                }))
+        );
+    }};
     $(".rv-item").click(generateMovie);
 
-    //shu's code
 
     var searchBarEl = document.querySelector(".search-bar-input");
     var searchDropdownEl = document.getElementById("search-dropdown");
@@ -101,41 +107,42 @@ function getHome(event) {
         fetch(requestThemoviedbURL, {
             method: 'GET',
         })
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            searchDropdownEl.innerHTML = '';
-            searchDropdownEl.classList.remove("hide");
-            // console.log(data)
-            if (searchBarEl.value) {
-                for (i = 0; i < 5; i++) {
-                    var dropDownItem = document.createElement("li");
-                    dropDownItem.classList.add("drop-down-item");
-                    dropDownItem.setAttribute("data-ID", data.results[i].id);
-                    dropDownItem.setAttribute("data-title", data.results[i].original_title);
-                
-                    var title = document.createElement("p");
-                    title.textContent = data.results[i].original_title;
-                    
-                
-                    var releaseDate = document.createElement("p");
-                    releaseDate.textContent = data.results[i].release_date.substring(0, 4);
-                
-                    var poster = document.createElement("img");
-                    if (data.results[i].poster_path) {
-                        poster.setAttribute("src", 'https://image.tmdb.org/t/p/w500' + data.results[i].poster_path);
-                    }
-                
-                    dropDownItem.append(title, releaseDate, poster);
-                    searchDropdownEl.append(dropDownItem);
-                
-                    title.addEventListener("click", getMovieID);
-                    releaseDate.addEventListener("click", getMovieID);
-                    poster.addEventListener("click", getMovieID);
-                };
-            }
-        });
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                searchDropdownEl.innerHTML = '';
+                searchDropdownEl.classList.remove("hide");
+                // console.log(data)
+                if (searchBarEl.value) {
+                    for (i = 0; i < 5; i++) {
+                        var dropDownItem = document.createElement("li");
+                        dropDownItem.classList.add("drop-down-item");
+                        dropDownItem.setAttribute("data-ID", data.results[i].id);
+                        dropDownItem.setAttribute("data-title", data.results[i].original_title);
+
+                        var title = document.createElement("p");
+                        title.textContent = data.results[i].original_title;
+
+
+                        var releaseDate = document.createElement("p");
+                        if (data.results[i].release_date) {
+                        releaseDate.textContent = data.results[i].release_date.substring(0, 4);}
+
+                        var poster = document.createElement("img");
+                        if (data.results[i].poster_path) {
+                            poster.setAttribute("src", 'https://image.tmdb.org/t/p/w500' + data.results[i].poster_path);
+                        }
+
+                        dropDownItem.append(title, releaseDate, poster);
+                        searchDropdownEl.append(dropDownItem);
+
+                        title.addEventListener("click", getMovieID);
+                        releaseDate.addEventListener("click", getMovieID);
+                        poster.addEventListener("click", getMovieID);
+                    };
+                }
+            });
     }
 
     function getMovieID(event) {
@@ -148,7 +155,7 @@ function getHome(event) {
 
 
     //basically load the poster cards using stored movie_ID and randomIndex when home button is clicked.
-    getRec(movieID, event);
+    if(movieID){getRec(movieID, event)};
 
 }
 
@@ -165,33 +172,36 @@ function getRec(movieID, event) {
     fetch(movieDBRecURL, {
         method: 'GET',
     })
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-    //when generating recommendation, if home button was clicked, we keep the same random Index effectively loading the same posters generated before
-    if(event){if (event.target.classList.contains("home-btn")) { generateCards(data) }
-    else {
-        getRandomIndex(data);
-        generateCards(data);
-    }}
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            //when generating recommendation, if home button was clicked, we keep the same random Index effectively loading the same posters generated before
+            if (event) {
+                if (event.target.classList.contains("home-btn")) { generateCards(data) }
+                else {
+                    getRandomIndex(data);
+                    generateCards(data);
+                }
+            }
 
-    function generateCards(data) {
-        for (i = 0; i < 4; i++) {
-            randomBank.push(randomBank[i]);
-            var posterDisplay = document.createElement("div");
-            posterDisplay.classList.add("card-container");
-            posterDisplay.innerHTML = "<img class='card-image' data-title='" + data.results[randomBank[i]].title + "' src=https://image.tmdb.org/t/p/w500" + data.results[randomBank[i]].poster_path + "> <p class='card-text'>" + data.results[randomBank[i]].title + "<p>";
-            posterDisplay.setAttribute("data-ID", data.results[randomBank[i]].id);
-            posterDisplay.setAttribute("data-title", data.results[randomBank[i]].title);
-            cardDeckEl.append(posterDisplay);
+            function generateCards(data) {
+                for (i = 0; i < 4; i++) {
+                    randomBank.push(randomBank[i]);
+                    var posterDisplay = document.createElement("div");
+                    posterDisplay.classList.add("card-container");
+                    var urlTitle = data.results[randomBank[i]].title.replaceAll(" ", "-");
+                    posterDisplay.innerHTML = "<a href='#" + urlTitle + "'> <img class='card-image' data-title='" + data.results[randomBank[i]].title + "' src=https://image.tmdb.org/t/p/w500" + data.results[randomBank[i]].poster_path + "></a> <p class='card-text'>" + data.results[randomBank[i]].title + "<p>";
+                    posterDisplay.setAttribute("data-ID", data.results[randomBank[i]].id);
+                    posterDisplay.setAttribute("data-title", data.results[randomBank[i]].title);
+                    cardDeckEl.append(posterDisplay);
 
-            posterDisplay.addEventListener("click", generateMovie);
+                    posterDisplay.addEventListener("click", generateMovie);
 
-        };
-    }
-    return randomBank;
-    });
+                };
+            }
+            return randomBank;
+        });
 }
 
 function getRandomIndex(data) {
